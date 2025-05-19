d9d001eb966dbaf840222bde6c1901149da0958c
joeeey <513358386@qq.com>
2025-05-19 17:44:36 +0800

    Update terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -80,4 +80,10 @@ variable "openai_api_key" {
   description = "OpenAI API key"
   type        = string
   sensitive   = true
+}
+
+variable "pinecone_api_key" {
+  description = "Pinecone API key for vector database"
+  type        = string
+  sensitive   = true
 } 
